ba781c2869c1460a7aa9c7f1d244a2142e62db04
umakrishna-2002 <129099526+umakrishna-2002@users.noreply.github.com>
2025-05-05 14:48:41 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+localhost                                                                                                                                                                                       sudo apt update
+sudo apt install openjdk-17-jdk -y
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
